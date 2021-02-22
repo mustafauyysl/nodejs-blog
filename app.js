5005498e9632e8d793d036ccde88bb92575ffa80
Mustafa Uysal <41873800+mustafauyysl@users.noreply.github.com>
2021-02-22 23:04:15 +0300

    added template engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 const hostName = "127.0.0.1";
@@ -6,16 +7,21 @@ const path = require("path");
 
 app.use(express.static("public"));
 
+// Template Engine
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "main/index.html"));
+  res.render("main/index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "main/about.html"));
+  res.render("main/about");
 });
 
 app.get("/blog", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "main/blog.html"));
+  res.render("main/blog");
 });
 
 app.listen(port, () => {
