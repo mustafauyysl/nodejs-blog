eabdb31d367192edf0f9749d6ca56faeed53116a
Mustafa Uysal <41873800+mustafauyysl@users.noreply.github.com>
2021-02-23 08:03:59 +0300

    added new blog logic

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 const hostName = "127.0.0.1";
-const path = require("path");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// MongoDB Connect
 
 mongoose.connect("mongodb://127.0.0.1/nodeblog_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Express Static Folder
 
 app.use(express.static("public"));
 
@@ -18,8 +22,17 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+// Body Parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+
 const main = require("./routes/main");
+const posts = require("./routes/posts");
 app.use("/", main);
+app.use("/posts", posts);
 
 app.listen(port, () => {
   console.log(`Server listening at http://${hostName}:${port}`);
